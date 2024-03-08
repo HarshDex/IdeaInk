bd89f12047aea6a2e708410660b00f3b31196079
HarshDex <nature4917@gmail.com>
2024-03-08 22:31:37 +0530

    changes

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -24,21 +24,21 @@ const Signup = () => {
 	};
 
 	return (
-		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+		<div className='flex flex-col  justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+				<h1 className='text-3xl font-semibold  text-gray-300 mb-5'>
 					Sign Up <span className='text-black'> White Board </span>
 				</h1>
 
 				<form onSubmit={handleSubmit}>
 					<div>
 						<label className='label p-2'>
-							<span className='text-base label-text'>Full Name</span>
+							<span className='text-1xl label-text'>Full Name</span>
 						</label>
 						<input
 							type='text'
 							placeholder='John Doe'
-							className='w-full input input-bordered  h-10'
+							className='w-full input input-bordered  h-10 pl-2 outline-none rounded-md mb-3'
 							value={inputs.fullName}
 							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
 						/>
@@ -46,12 +46,12 @@ const Signup = () => {
 
 					<div>
 						<label className='label p-2 '>
-							<span className='text-base label-text'>Username</span>
+							<span className='text-base label-text '>Username</span>
 						</label>
 						<input
 							type='text'
 							placeholder='johndoe'
-							className='w-full input input-bordered h-10'
+							className='w-full input input-bordered h-10 pl-2 outline-none rounded-md mb-3'
 							value={inputs.username}
 							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
 						/>
@@ -64,7 +64,7 @@ const Signup = () => {
 						<input
 							type='password'
 							placeholder='Enter Password'
-							className='w-full input input-bordered h-10'
+							className='w-full input input-bordered h-10 pl-2 outline-none rounded-md mb-3'
 							value={inputs.password}
 							onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
 						/>
@@ -77,7 +77,7 @@ const Signup = () => {
 						<input
 							type='password'
 							placeholder='Confirm Password'
-							className='w-full input input-bordered h-10'
+							className='w-full input input-bordered h-10 pl-2 outline-none rounded-md mb-3'
 							value={inputs.confirmPassword}
 							onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
 						/>
